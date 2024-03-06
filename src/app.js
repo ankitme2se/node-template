47ef3c10b7d25d4715b084c0df3080382cc2bd13
ankitme2se <96779229+ankitme2se@users.noreply.github.com>
2024-03-06 11:25:30 +0530

    test husky

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     logger.info('/, api called');
     // const err = createError(401, 'You are not allowed to access this page');
     // throw err;
-    res.send('Welcome to auth-service');
+    res.send(`Welcome to Ankit's Project`);
 });
 
 app.use('/auth', authRouter);
